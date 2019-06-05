f697ae5a7be152c695d5c397cb1d036f67b6f9cf
Brent Baumgartner <brent@brentbaum.com>
2019-06-05 17:02:51 -0400

    Update bio

--- blog/src/components/bio.js
+++ blog/src/components/bio.js
@@ -38,8 +38,7 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong> who lives and works in Charlottesville at TwinThread, building data-driven products.
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
                 You should follow him on Twitter
