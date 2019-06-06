c275a7c5ab706def4a6bb982b210afa3d56a0824
Brent Baumgartner <brent@brentbaum.com>
2019-06-05 23:20:50 -0400

    Add extra logging

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
+echo "Starting build"
 cd notion
 [ -e notion/posts ] && rm -r notion/posts
+echo "Install deps"
+pipenv install
+echo "Get posts"
 pipenv run python get_posts.py
 cd ../
 [ -e blog/content/blog ] && rm -r blog/content/blog
 cp -r notion/posts blog/content/blog
 cd blog
+echo "Build frontend site"
 yarn build
