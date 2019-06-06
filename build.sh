d60d1cc646e072d97abda25677fb95dc82ba3eda
Brent Baumgartner <brent@brentbaum.com>
2019-06-05 23:30:54 -0400

    Install pipenv as part of the build process

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
+pip install pipenv
 echo "Starting build"
 cd notion
+pipenv install
 [ -e notion/posts ] && rm -r notion/posts
 echo "Get posts"
-python get_posts.py
+pipenv run python get_posts.py
 cd ../
 [ -e blog/content/blog ] && rm -r blog/content/blog
 cp -r notion/posts blog/content/blog
