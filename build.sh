01e3faa32b04a0ad5cceffbfa545ca2b0e6db6c5
Brent Baumgartner <brent@brentbaum.com>
2019-06-05 23:26:42 -0400

    Specify requirements using good ol' pip requirements.txt

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 echo "Starting build"
 cd notion
 [ -e notion/posts ] && rm -r notion/posts
-echo "Install deps"
-pipenv install
 echo "Get posts"
-pipenv run python get_posts.py
+python get_posts.py
 cd ../
 [ -e blog/content/blog ] && rm -r blog/content/blog
 cp -r notion/posts blog/content/blog
