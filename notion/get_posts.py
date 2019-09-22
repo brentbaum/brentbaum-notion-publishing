c5dc8000c497a05c8601c508a2fe793d05dbf326
Brent Baumgartner <brent@brentbaum.com>
2019-09-22 17:03:45 -0400

    Fix broken API route

--- notion/get_posts.py
+++ notion/get_posts.py
@@ -62,22 +62,31 @@ def resolve_fields(post_record):
 
 
 def get_blog_posts():
-    page = client.get_block("https://www.notion.so/2e831e83a61846ccbf184c0f00753549")
+    # Get link by clicking on ... above collection.
+    collection_link = "https://www.notion.so/2e831e83a61846ccbf184c0f00753549?v=0483f7a0d6ed4919a537bfd443a07022"
+    collection_view = client.get_collection_view(collection_link)
+    all_rows = list(collection_view.build_query().execute())
+    print("Fetched all posts, formatting for consumption.")
 
     return [
         resolve_fields(post)
-        for post in page.collection.get_rows()
+        for post in all_rows
         if post.get_property("published")
     ]
 
 
 if __name__ == "__main__":
+    print("start")
     try:
         os.mkdir("posts/")
     except:
         pass
+    posts = get_blog_posts()
+    print(len(posts), "posts found")
     for post in get_blog_posts():
+        print("Writing post", post["slug"])
         filename = post["slug"] + "/index.md"
         os.mkdir("posts/" + post["slug"])
         with open("posts/" + filename, "w") as text_file:
             text_file.write(post["header"] + post["markdown"])
+    print("Finished fetching posts.")
